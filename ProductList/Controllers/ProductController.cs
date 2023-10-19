85d8bc5470fbb018a004336c12453b465d0c06a1
kemal <stormanq37@gmail.com>
2023-10-19 15:06:48 +0300

    product details sayfası yapıldı

---
+++ ProductList/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductList.Models;
+using ProductList.Services;
+
+namespace ProductList.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ProductService _productService;
+
+        public ProductController(ProductService productService)
+        {
+            _productService = productService;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> ProductDetails(string productId) {
+
+            Product product = await _productService.GetProductAsync(productId);
+
+             if (product == null)
+            {
+                return NotFound(); // 404 Not Found sayfasına yönlendirilebilir
+            }
+             return View(product);
+        }
+    }
+}
